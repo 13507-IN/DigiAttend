1f85e6f32ed6e3246bf40582ed9a3bd5837e77b9
Rishiraj <rishirajnatj@gmail.com>
2026-05-17 18:35:42 +0530

    Add configuration and attendance controller TypeScript declarations

---
+++ backend/dist/controllers/attendanceController.d.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+export declare const attendanceController: {
+    recordAttendance(req: Request, res: Response): Promise<void>;
+    getAttendanceByCourse(req: Request, res: Response): Promise<void>;
+    getStudentAttendance(req: Request, res: Response): Promise<void>;
+};
+//# sourceMappingURL=attendanceController.d.ts.map
